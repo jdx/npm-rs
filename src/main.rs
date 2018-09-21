b686b3b5e832a417acacd007997164739226a96a
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-21 10:00:53 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,42 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use hyper::rt::{self, Future, Stream};
+use hyper::Client;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Write};
-use hyper::Client;
-use hyper::rt::{self, Future, Stream};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct PackageLockDependency {
+    version: String,
+    resolved: String,
+    integrity: String,
+    requires: Option<HashMap<String, String>>,
+}
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct PackageLock {
     requires: bool,
     lockfile_version: u8,
+    dependencies: HashMap<String, PackageLockDependency>,
 }
 
 fn readlock() -> std::io::Result<()> {
     let mut file = File::open("fixtures/package-lock.json")?;
     let lock: PackageLock = serde_json::from_reader(&mut file)?;
     println!("version {}", lock.lockfile_version);
+    let dependency = lock.dependencies.get("ansi-styles").unwrap();
+    println!(
+        "tarball {}@{}: {}",
+        "ansi-styles", dependency.version, dependency.resolved
+    );
     Ok(())
 }
 
-fn testhttp() -> impl Future<Item=(), Error=()> {
+fn testhttp() -> impl Future<Item = (), Error = ()> {
     let uri = "http://httpbin.org/ip".parse().unwrap();
     let client = Client::new();
 
@@ -32,7 +48,8 @@ fn testhttp() -> impl Future<Item=(), Error=()> {
         .get(uri)
         .and_then(|res| {
             res.into_body().for_each(|chunk| {
-                io::stdout().write_all(&chunk)
+                io::stdout()
+                    .write_all(&chunk)
                     .map_err(|e| panic!("example expects stdout is open, error={}", e))
             })
         })
