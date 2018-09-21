b3e29a3bafd288d6c47e53f0041928731b134cfc
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-21 00:02:26 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+extern crate hyper;
 extern crate serde;
 extern crate serde_json;
 
@@ -6,7 +6,9 @@ extern crate serde_json;
 extern crate serde_derive;
 
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{self, Write};
+use hyper::Client;
+use hyper::rt::{self, Future, Stream};
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -15,11 +17,32 @@ struct PackageLock {
     lockfile_version: u8,
 }
 
-fn main() -> std::io::Result<()> {
+fn readlock() -> std::io::Result<()> {
     let mut file = File::open("fixtures/package-lock.json")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let lock: PackageLock = serde_json::from_str(&mut contents)?;
+    let lock: PackageLock = serde_json::from_reader(&mut file)?;
     println!("version {}", lock.lockfile_version);
+    Ok(())
+}
+
+fn testhttp() -> impl Future<Item=(), Error=()> {
+    let uri = "http://httpbin.org/ip".parse().unwrap();
+    let client = Client::new();
+
+    client
+        .get(uri)
+        .and_then(|res| {
+            res.into_body().for_each(|chunk| {
+                io::stdout().write_all(&chunk)
+                    .map_err(|e| panic!("example expects stdout is open, error={}", e))
+            })
+        })
+        .map_err(|err| {
+            println!("Error: {}", err);
+        })
+}
+
+fn main() -> std::io::Result<()> {
+    readlock()?;
+    rt::run(testhttp());
     Ok(())
 }
