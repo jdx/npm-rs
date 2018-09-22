328fd69007fce2d3e456b2217bdb382b9fb69d47
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-22 11:29:55 -0700

    download all base packages

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,10 @@ extern crate tar;
 extern crate serde_derive;
 
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::copy;
-use std::io::Error;
+use std::fs;
+use std::io;
 // use std::io::{self, Write};
-// use std::path::Path;
+use std::path::Path;
 // use tar::Archive;
 
 #[derive(Serialize, Deserialize)]
@@ -32,13 +31,21 @@ struct PackageLock {
 }
 
 fn readlock() -> std::io::Result<PackageLock> {
-    let mut file = File::open("fixtures/package-lock.json")?;
+    let mut file = fs::File::open("fixtures/package-lock.json")?;
     let lock: PackageLock = serde_json::from_reader(&mut file)?;
     println!("package-lock.json version: {}", lock.lockfile_version);
     Ok(lock)
 }
 
-fn download_file(url: &String, to: &String) -> Result<(), Error> {
+fn download_tarballs(lock: PackageLock) -> Result<(), io::Error> {
+    for (name, dep) in lock.dependencies.iter() {
+        println!("{} {}", name, dep.resolved);
+        download_file(&dep.resolved, &format!("tmp/{}.tgz", name))?;
+    }
+    Ok(())
+}
+
+fn download_file(url: &String, to: &String) -> Result<(), io::Error> {
     println!("downloading {} to {}", url, to);
     let mut response = reqwest::get(url).expect("http failed");
     response
@@ -47,17 +54,14 @@ fn download_file(url: &String, to: &String) -> Result<(), Error> {
         .and_then(|segments| segments.last())
         .and_then(|name| if name.is_empty() { None } else { Some(name) })
         .expect("download failed");
-    copy(&mut response, &mut File::create(to)?)?;
+    let dir = Path::new(to).parent().unwrap();
+    fs::create_dir_all(dir)?;
+    io::copy(&mut response, &mut fs::File::create(to)?)?;
     Ok(())
 }
 
 fn main() -> std::io::Result<()> {
     let lock = readlock()?;
-    let dependency = lock.dependencies.get("ansi-styles").unwrap();
-    println!(
-        "tarball: {}@{}: {}",
-        "ansi-styles", dependency.version, dependency.resolved
-    );
-    download_file(&dependency.resolved, &String::from("tarball.tgz"))?;
+    download_tarballs(lock)?;
     Ok(())
 }
