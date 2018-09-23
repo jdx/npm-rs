393f65f7ef44aee111aeec81307e4ec1d4b6146d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-23 09:45:38 -0700

    parse subdeps

--- src/package_lock.rs
+++ src/package_lock.rs
@@ -7,6 +7,14 @@ use std::convert::AsRef;
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+pub struct PackageLock {
+    pub requires: bool,
+    pub lockfile_version: u8,
+    pub dependencies: HashMap<String, PackageLockDependency>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct PackageLockDependency {
     #[serde(skip_serializing)]
     name: Option<String>,
@@ -15,6 +23,7 @@ pub struct PackageLockDependency {
     pub resolved: String,
     pub integrity: String,
     pub requires: Option<HashMap<String, String>>,
+    pub dependencies: Option<HashMap<String, PackageLockDependency>>,
 }
 
 impl PackageLockDependency {
@@ -27,14 +36,6 @@ impl PackageLockDependency {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct PackageLock {
-    pub requires: bool,
-    pub lockfile_version: u8,
-    pub dependencies: HashMap<String, PackageLockDependency>,
-}
-
 impl PackageLock {
     pub fn new<T: AsRef<Path>>(root: T) -> PackageLock {
         let path = Path::new(root.as_ref()).join("package-lock.json");
@@ -42,9 +43,16 @@ impl PackageLock {
         let mut lock: PackageLock = serde_json::from_reader(&mut file).expect("invalid package-lock");
         println!("package-lock.json version: {}", lock.lockfile_version);
 
-        for (name, mut dep) in &mut lock.dependencies {
-            dep.name = Some(name.clone());
-        }
+        fn init_dep (dependencies: &mut HashMap<String, PackageLockDependency>) {
+            for (name, mut dep) in dependencies {
+                dep.name = Some(name.clone());
+                match dep.dependencies {
+                    Some(ref mut deps) => init_dep(deps),
+                    None => (),
+                }
+            }
+        };
+        init_dep(&mut lock.dependencies);
 
         lock
     }
