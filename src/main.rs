b8b42ca709e43bd1b96eb02b0046f91334cd13c5
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-22 17:17:09 -0700

    cache shit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate digest;
+extern crate xx;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate sha1;
 extern crate tar;
 
 #[macro_use]
@@ -9,9 +12,7 @@ extern crate serde_derive;
 use std::collections::HashMap;
 use std::fs;
 use std::io;
-// use std::io::{self, Write};
 use std::path::Path;
-// use tar::Archive;
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -39,14 +40,44 @@ fn readlock() -> std::io::Result<PackageLock> {
 
 fn download_tarballs(lock: PackageLock) -> Result<(), io::Error> {
     for (name, dep) in lock.dependencies.iter() {
-        println!("{} {}", name, dep.resolved);
-        download_file(&dep.resolved, &format!("tmp/{}.tgz", name))?;
+        let file = Path::new("tmp")
+            .join(name)
+            .join(format!("{}-{}.tgz", name, dep.version));
+        let s: Vec<&str> = dep.integrity.splitn(2, "-").collect();
+        let method = s[0];
+        let expected = xx::base64::decode_hex(s[1]).unwrap();
+        let get_sha = |hasher: fn(&str) -> Result<String, io::Error>| -> Option<String> {
+            match hasher(&file.to_str().unwrap()) {
+                Ok(v) => Some(v),
+                Err(_) => None,
+            }
+        };
+        let actual = match method {
+            "sha1" => get_sha(xx::hash::file_sha1),
+            "sha256" => get_sha(xx::hash::file_sha256),
+            "sha512" => get_sha(xx::hash::file_sha512),
+            _ => panic!("Unexpected method {}", method),
+        };
+        match actual {
+            Some(actual) => {
+                if actual != expected {
+                    println!("{:?}", actual);
+                    println!("{:?}", expected);
+                    println!("hash fail: expected hash of {:?}:{} to be {}", file, actual, expected);
+                    download_file(&dep.resolved, &file)?;
+                }
+            },
+            None => {
+                println!("file not found: {:?}", file);
+                download_file(&dep.resolved, &file)?;
+            }
+        }
     }
     Ok(())
 }
 
-fn download_file(url: &String, to: &String) -> Result<(), io::Error> {
-    println!("downloading {} to {}", url, to);
+fn download_file(url: &String, to: &Path) -> Result<(), io::Error> {
+    println!("downloading {} to {}", url, to.to_str().unwrap());
     let mut response = reqwest::get(url).expect("http failed");
     response
         .url()
@@ -54,8 +85,7 @@ fn download_file(url: &String, to: &String) -> Result<(), io::Error> {
         .and_then(|segments| segments.last())
         .and_then(|name| if name.is_empty() { None } else { Some(name) })
         .expect("download failed");
-    let dir = Path::new(to).parent().unwrap();
-    fs::create_dir_all(dir)?;
+    fs::create_dir_all(to.parent().unwrap())?;
     io::copy(&mut response, &mut fs::File::create(to)?)?;
     Ok(())
 }
