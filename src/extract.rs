24266ae4d914ea59aa5f7b4e1b3528ed87dcb105
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-22 22:51:14 -0700

    added extracting

--- src/extract.rs
+++ src/extract.rs
@@ -1,11 +1,15 @@
+extern crate flate2;
 extern crate tar;
 extern crate xx;
 
+use self::flate2::read::GzDecoder;
+use self::tar::Archive;
 use self::xx::hash;
 use package_lock::PackageLock;
+use std::fs;
+use std::fs::File;
 use std::io;
-use std::path::Path;
-// use tar::Archive;
+use std::path::{Path, PathBuf};
 
 pub fn verify_tarball(path: &Path, integrity: &String, must: bool) -> Result<bool, io::Error> {
     let s: Vec<&str> = integrity.splitn(2, '-').collect();
@@ -18,15 +22,44 @@ pub fn verify_tarball(path: &Path, integrity: &String, must: bool) -> Result<boo
         _ => panic!("Unexpected method {}", method),
     }?;
     let m = actual == expected;
-    if ! m && must {
-        panic!("hash mismatch path: {:?}\nexpected: {}\nactual: {}", path, expected, actual);
+    if !m && must {
+        panic!(
+            "hash mismatch path: {:?}\nexpected: {}\nactual: {}",
+            path, expected, actual
+        );
     }
 
     Ok(m)
 }
 
-pub fn extract_tarballs(lock: &PackageLock) {
-    for (_, dep) in &lock.dependencies {
-        verify_tarball(&dep.cache_path(), &dep.integrity, true).unwrap();
+pub fn extract_tarballs<P: AsRef<Path>>(lock: &PackageLock, _root: P) {
+    let root = _root.as_ref();
+    for (name, dep) in &lock.dependencies {
+        let tarball = &dep.cache_path();
+        verify_tarball(tarball, &dep.integrity, true).unwrap();
+        println!("unpacking {:?}", tarball);
+        let mut file = GzDecoder::new(File::open(tarball).unwrap());
+        let mut archive = Archive::new(file);
+        let base = root.join("node_modules");
+        for file in archive.entries().unwrap() {
+            let mut file = file.unwrap();
+            let path = get_real_path(name, file.path().unwrap().as_ref(), &base);
+            println!("{:?}", path);
+            fs::create_dir_all(path.parent().unwrap()).unwrap();
+            let mut output = File::create(&path).unwrap();
+            io::copy(&mut file, &mut output).unwrap();
+        }
     }
+}
+
+fn get_real_path(name: &str, path: &Path, root: &PathBuf) -> PathBuf {
+    let orig_path = path;
+    let path = root
+        .join(name)
+        .join(orig_path.strip_prefix("package").unwrap());
+    if !path.starts_with(root) {
+        panic!("invalid tarball");
+    }
+
+    path
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ mod package_lock;
 use download::download_tarballs;
 use package_lock::PackageLock;
 use extract::extract_tarballs;
+use std::fs;
 
 fn main() {
-    let lock = PackageLock::new("fixtures");
+    let root = fs::canonicalize("fixtures").unwrap();
+    let lock = PackageLock::new(&root);
     download_tarballs(&lock);
-    extract_tarballs(&lock);
+    extract_tarballs(&lock, &root);
 }
